58379475882547c0f530e2cf0aece9b7a59712c8
Ironboy <ironboy@users.noreply.github.com>
2022-06-03 01:44:04 +0200

    simpler

--- docker-tools-template/copy-to-docker-container/git-cloner.js
+++ docker-tools-template/copy-to-docker-container/git-cloner.js
@@ -176,17 +176,6 @@ function buildComposeFile() {
     port = hostPort || port;
 
     if (fs.existsSync(`/storage/branches/${branch}/Dockerfile`)) {
-      let bind = gitBranchName === branch;
-      if (bind) {
-        // Binding from Docker in Docker to a volume on windows
-        // does not seem to work...
-        // (even if we re-parse the path to posix/linux)
-        // so let us create the container with a bind-mount
-        // later in our start.sh script
-        fs.unlinkSync('./bind-mounted-container-port.txt');
-        fs.writeFileSync('./bind-mounted-container-port.txt', port, 'utf-8');
-        continue;
-      }
       let name = gitRepoName + '-' + branch;
       let workingDir = `/storage/branches/${branch}`;
       yml = [
